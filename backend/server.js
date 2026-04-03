748ff0f597a599315f4cbe42106387f4a356dfbe
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-04-04 00:28:34 +0530

    Fix Node Modules issues

--- backend/server.js
+++ backend/server.js
@@ -6,5 +6,5 @@ const connectToDb = require("./src/config/database");
 
 connectToDb();
 
-app.listen(3000,()=>console.log("server is running on port 3000"));
+app.listen(3000,()=>console.log("server is running on port 3000!"));
 
